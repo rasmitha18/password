40b757edb1bdb0a70a55de208f293d4daeab5847
unknown <rasmithadev@gmail.com>
2026-07-30 06:48:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Rasmitha from "./Rasmitha";
+function App() {
+  
+  return (
+    <div>
+      <Rasmitha name="rasmitha"
+      course="Btech"
+      clg="snit"
+      />
+<br/>
+      <Rasmitha name="raju"
+      course="mtech"
+      clg="snit"
+      />
+<br/>
+      <Rasmitha name="maha"
+      course="Btech"
+      clg="snit"
+      />
+
+   
+
+  </div>
+  );
+}
+export default App;
---
+++ src/Rasmitha.jsx
@@ -0,0 +1,10 @@
+function Rasmitha(props){
+    return (
+        <div>
+            <h1>Name:{props.name}</h1>
+            <h1>course:{props.course}</h1>
+            <h1>clg:{props.clg}</h1>
+        </div>
+    )
+}
+export default Rasmitha
